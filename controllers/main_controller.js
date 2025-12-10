11c86a83d8fe84b9a33101002a305e641f196b35
MariaMedina <mariawestengen@icloud.com>
2025-12-10 10:35:41 +0100

    created index page and find Data page. Did css for index page

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -4,6 +4,11 @@ const rdf = require('../rdf/rdf.js')
 const index_render = (req,res)=>{
     res.render("index")
 }
+
+const findData_render = (req, res) => {
+    res.render("findData");
+}
+
 const rdf_render = async (req,res)=>{
     const rdfinfo = await rdf.getQuadRDF("https://fellesdatakatalog.digdir.no/datasets/3b6cb3a2-8211-3564-a576-4047c6f614ab")
     res.render("tempRDF", { rdf: rdfinfo })
@@ -12,5 +17,6 @@ const rdf_render = async (req,res)=>{
 
 module.exports = {
     index_render,
+    findData_render,
     rdf_render
 }
