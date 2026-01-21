e7a7dd8d65da13c1d31159f527ae1fe82bc802de
MariaMedina <mariawestengen@icloud.com>
2026-01-21 11:32:50 +0100

    working on findDatapreviews

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -8,8 +8,9 @@ const index_render = (req,res)=>{
     res.render("index")
 }
 
-const findData_render = (req, res) => {
-    res.render("findData");
+const findData_render = async (req, res) => {
+    const rdfinfo = await rdf.getRDF("https://fellesdatakatalog.digdir.no/datasets/3b6cb3a2-8211-3564-a576-4047c6f614ab")
+    res.render("findData", {rdf: rdfinfo});
 }
 
 const rdf_render = async (req,res)=>{
