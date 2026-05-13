6bfa778a92b5c904603adac6dfbb0a17606729e5
danibir <daniel.s.birkeland@gmail.com>
2026-05-13 11:47:17 +0200

    Fixing other tiny fuckass render error

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -28,7 +28,7 @@ const findData_render = async (req, res) => {
   const query = req.query.search || "";
   const sort = req.query.sort || "";
   if (!req.isDBConnected) {
-    return res.render("findData", { rdf: [], rdfList: [], sort });
+    return res.render("findData", { data: [], sort });
   }
   let data = [];
 
